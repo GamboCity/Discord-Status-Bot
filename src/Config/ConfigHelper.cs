14e0b6200ffe710edd3bcff3bc37742918393225
Keyinator <k3yinator@gmail.com>
2024-11-11 02:36:46 +0100

    feat: Implement Microsoft.Extensions.Logging.ILogger

--- src/Config/ConfigHelper.cs
+++ src/Config/ConfigHelper.cs
@@ -1,28 +1,29 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace GamboCity_DiscordBot.Config;
 
-public static class ConfigHelper {
-    public static bool IsEnvironmentValid(IConfiguration configuration) {
+public class ConfigHelper(IConfiguration configuration, ILogger<ConfigHelper> logger) {
+    public bool IsEnvironmentValid() {
         // Check if the DISCORD_TOKEN environment variable is set correctly
         string? discordToken = configuration["DISCORD_TOKEN"];
         if (discordToken is null) {
-            Console.WriteLine("DISCORD_TOKEN environment variable is not set.");
+            logger.LogError("DISCORD_TOKEN environment variable is not set.");
             return false;
         }
         if (discordToken == "REPLACE_ME") {
-            Console.WriteLine("DISCORD_TOKEN environment variable is still default.");
+            logger.LogError("DISCORD_TOKEN environment variable is still default.");
             return false;
         }
 
         // Check if the FIVEM_URL environment variable is set correctly
         string? fivemUrl = configuration["FIVEM_URL"];
         if (fivemUrl is null) {
-            Console.WriteLine("FIVEM_URL environment variable is not set.");
+            logger.LogError("FIVEM_URL environment variable is not set.");
             return false;
         }
         if (fivemUrl == "https://REPLACE_ME/dynamic.json") {
-            Console.WriteLine("FIVEM_URL environment variable is still default.");
+            logger.LogError("FIVEM_URL environment variable is still default.");
             return false;
 
         }
@@ -30,15 +31,15 @@ public static class ConfigHelper {
         // Check if the UPDATE_INTERVAL environment variable is set correctly
         string? updateInterval = configuration["UPDATE_INTERVAL"];
         if (updateInterval is null) {
-            Console.WriteLine("UPDATE_INTERVAL environment variable is not set.");
+            logger.LogError("UPDATE_INTERVAL environment variable is not set.");
             return false;
         }
         if (!int.TryParse(updateInterval, out int updateIntervalNum)) {
-            Console.WriteLine("UPDATE_INTERVAL environment variable is not a valid number.");
+            logger.LogError("UPDATE_INTERVAL environment variable is not a valid number.");
             return false;
         }
         if (updateIntervalNum < 1) {
-            Console.WriteLine("UPDATE_INTERVAL environment variable must be greater than or equal to 1.");
+            logger.LogError("UPDATE_INTERVAL environment variable must be greater than or equal to 1.");
             return false;
         }
 
--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,7 @@ using GamboCity_DiscordBot.Config;
 using GamboCity_DiscordBot.src.Modules;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System.Globalization;
 
 namespace GamboCity_DiscordBot.src {
@@ -13,27 +14,34 @@ namespace GamboCity_DiscordBot.src {
         };
 
         private readonly IConfiguration Configuration;
-
         private readonly IServiceProvider Services;
+        private readonly ILogger logger;
 
         public Program() {
             Configuration = new ConfigurationBuilder()
                 .AddEnvironmentVariables()
                 .Build();
 
-            if (!ConfigHelper.IsEnvironmentValid(Configuration))
-                Environment.Exit(1);
-
             Services = new ServiceCollection()
+                .AddLogging(configure => {
+                    configure.AddConsole();
+                })
                 .AddSingleton(Configuration)
                 .AddSingleton(SocketConfig)
+                .AddSingleton<ConfigHelper>()
                 .AddSingleton<DiscordSocketClient>()
                 .AddSingleton<DiscordStatus>()
                 .BuildServiceProvider();
+
+            logger = Services.GetRequiredService<ILogger<Program>>();
         }
 
         public static async Task Main(string[] _) {
             Program program = new();
+
+            ConfigHelper configHelper = program.Services.GetRequiredService<ConfigHelper>();
+            if (!configHelper.IsEnvironmentValid())
+                Environment.Exit(1);
 
             SetLanguage(program.Configuration["LANGUAGE"]!);
 
@@ -62,7 +70,7 @@ namespace GamboCity_DiscordBot.src {
         }
 
         private Task LogAsync(LogMessage message) {
-            Console.WriteLine(message.ToString());
+            logger.LogInformation(message.ToString());
             return Task.CompletedTask;
         }
 
